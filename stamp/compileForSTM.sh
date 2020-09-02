2edfec96d518e319ac76b20ddfcb2bf52f9a44cc
Douglas Pasqualin <douglas.pasqualin@gmail.com>
2020-09-02 19:13:29 -0300

    initial commit

---
+++ stamp/compileForSTM.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+for dir in \
+    bayes \
+    genome \
+    intruder \
+    kmeans \
+    labyrinth \
+    ssca2 \
+    vacation \
+    yada
+do
+
+cd ${dir}
+make -f Makefile.stm clean
+make -f Makefile.stm.otm
+cd ..
+
+done
+
+
+
+for dir in \
+    hashmap \
+    redblacktree 
+do
+
+cd ${dir}
+make -f Makefile clean
+make -f Makefile
+cd ..
+
+done
+
+
